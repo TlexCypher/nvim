3e97cdde999c04757a7a87308c62d550ba5b887d
Taichi Araki <araki130710@gmail.com>
2025-03-08 17:25:23 +0900

    feat: add some remap, <C-c> is alias to Escape etc.

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -5,6 +5,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- remain cursor to same place
+vim.keymap.set("n", "J", "mzJ`z")
+
 --search and fix cursor into center of screen.
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
@@ -17,6 +20,8 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
 --save and format
 vim.keymap.set("n", "<C-s>", function()
     vim.lsp.buf.format()
@@ -26,3 +31,7 @@ end)
 --replace macro
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- jump half screen but cursor stll remains center of screen.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
