f8d60aaae412422faaf73e37b033c3520038bef3
Taichi Araki <araki130710@gmail.com>
2025-03-08 17:25:00 +0900

    feat: Add some configurations to make outof popup menu criteria trasparent.

--- lua/after/plugin/colors.lua
+++ lua/after/plugin/colors.lua
@@ -6,6 +6,7 @@ function ColorMyPencils(color)
     -- color = color or "catppuccin"
     -- vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
